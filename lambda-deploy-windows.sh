570709f9582c322e029882dc91e84a4df1b58f78
Wesley Yep <wesleyyep@hotmail.com>
2017-05-29 21:52:47 +1200

    lex deploy bot script

--- lambda-deploy-windows.sh
+++ lambda-deploy-windows.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-echo running deployment script....
+echo running lambda deployment script....
 cd lambda
 ../zip.exe -r ../lambda.zip * # replace with just "zip" if using a mac
 cd ..
-if [ $1 = "prod" ]; then
-    echo deploying to prod
+if [ "$1" = "prod" ]; then
+    echo deploying lambda to prod
     aws lambda update-function-code --zip-file fileb://lambda.zip --function-name moviebotFunctionProd
 else
-    echo deploying to dev
+    echo deploying lambda to dev
     aws lambda update-function-code --zip-file fileb://lambda.zip --function-name moviebotFunction
 fi
