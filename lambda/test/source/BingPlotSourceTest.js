aa304ca1eef76f7dbd34cede5401d073b70107ac
Wesley Yep <wesleyyep@hotmail.com>
2017-07-02 14:19:34 +1200

    unit tests and travis calls lex tests too

--- lambda/test/source/BingPlotSourceTest.js
+++ lambda/test/source/BingPlotSourceTest.js
@@ -45,14 +45,67 @@ describe('BingSourceTest', function() {
         })
     });
 
-    // bingSource.getMovies("guy throws ring into volcano");
-    // bingSource.getMovies("jedi blows up death star");
-    // bingSource.getMovies("magician robbing bank");
-    // bingSource.getMovies("infectious disease that caused everyone to be zombie");
-    // bingSource.getMovies("wolverine travels back in time to save mutants");
-    // bingSource.getMovies("seven cowboys gathered to save a village from a tyrant");
-    // bingSource.getMovies("4 turtles and a rat");
-    // bingSource.getMovies("plane crash and man is alone on an island");
-    // bingSource.getMovies("apes riding horses");
+    it('should find Lord of the rings', function() {
+        this.timeout(5000);
+        return bingSource.getMovies("guy throws ring into volcano").then((movies) => {
+            expect(movies.find((m) => {return m.getTitle() === "The Lord of the Rings: The Fellowship of the Ring"})).to.not.equal(undefined)
+        })
+    });
+
+    it('should find Star Wars: Episode IV', function() {
+        this.timeout(5000);
+        return bingSource.getMovies("jedi blows up death star").then((movies) => {
+            expect(movies.find((m) => {return m.getTitle() === "Star Wars"})).to.not.equal(undefined)
+        })
+    });
+
+    it('should find Now You See Me', function() {
+        this.timeout(5000);
+        return bingSource.getMovies("magician robbing bank").then((movies) => {
+            expect(movies.find((m) => {return m.getTitle() === "Now You See Me"})).to.not.equal(undefined)
+        })
+    });
+
+    it('should find Contagion', function() {
+        this.timeout(5000);
+        return bingSource.getMovies("infectious disease that caused everyone to be zombie").then((movies) => {
+            expect(movies.find((m) => {return m.getTitle() === "Contagion"})).to.not.equal(undefined)
+        })
+    });
+
+    it('should find X-men DOFP', function() {
+        this.timeout(5000);
+        return bingSource.getMovies("wolverine travels back in time to save mutants").then((movies) => {
+            expect(movies.find((m) => {return m.getTitle() === "X-Men: Days of Future Past"})).to.not.equal(undefined)
+        })
+    });
+
+    it('should find The Magnificent Seven', function() {
+        this.timeout(5000);
+        return bingSource.getMovies("seven cowboys gathered to save a village from a tyrant").then((movies) => {
+            expect(movies.find((m) => {return m.getTitle() === "The Magnificent Seven"})).to.not.equal(undefined)
+        })
+    });
+
+    it('should find TMNT', function() {
+        this.timeout(5000);
+        return bingSource.getMovies("4 turtles and a rat").then((movies) => {
+            expect(movies.find((m) => {return m.getTitle() === "Teenage Mutant Ninja Turtles"})).to.not.equal(undefined)
+        })
+    });
+
+    it('should find Cast Away', function() {
+        this.timeout(5000);
+        return bingSource.getMovies("plane crash and man is alone on an island").then((movies) => {
+            expect(movies.find((m) => {return m.getTitle() === "Cast Away"})).to.not.equal(undefined)
+        })
+    });
+
+    it('should find Dawn of the Planet of the Apes', function() {
+        this.timeout(5000);
+        return bingSource.getMovies("apes riding horses").then((movies) => {
+            expect(movies.find((m) => {return m.getTitle() === "Dawn of the Planet of the Apes"})).to.not.equal(undefined)
+        })
+    });
 
 });
