a4ac586eb10c9035aae6d7a5b3cc9f0d16c183e6
Wal8800 <johnlaw1234@hotmail.com>
2017-07-09 22:02:51 +1200

    Fix test always passing

--- lambda/test/hydrator/HydratorTest.js
+++ lambda/test/hydrator/HydratorTest.js
@@ -5,16 +5,18 @@ var movieHydrator = require('../../hydrator/Hydrator')
 describe('HydratorTest', function() {
     describe('#hydrate', function() {
         it('should fill a movie object that have a title only', function() {
+            this.timeout(5000);
+
             var testMovieList = []
             var testMovie = movieBuilder.builder('Man of Steel').build();
             testMovieList.push(testMovie)
 
-            movieHydrator.hydrate(testMovieList).then( (movieList) => {
+            return movieHydrator.hydrate(testMovieList).then( (movieList) => {
                 expect(movieList[0].getTitle()).to.be.equal('Man of Steel')
                 expect(movieList[0].getReleaseDate()).to.be.equal('2013-06-12')
                 expect(movieList[0].getDirector()).to.be.equal('Zack Snyder')
                 expect(movieList[0].getTrailerUrl()).to.be.equal('https://www.youtube.com/watch?v=KVu3gS7iJu4')
-                expect(movieList[0].getTrailerThumbnail).to.be.equal('https://i.ytimg.com/vi/KVu3gS7iJu4/hqdefault.jpg')
+                expect(movieList[0].getTrailerThumbnail()).to.be.equal('https://i.ytimg.com/vi/KVu3gS7iJu4/hqdefault.jpg')
             }).catch((err) => {
                 throw new Error(err)
             })
